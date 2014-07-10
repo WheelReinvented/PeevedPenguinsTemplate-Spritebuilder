589d51e8a5fce0fffb1a843e9590adefeb5eeac7
Michal Wierzbinski <michal.wierzbinski@gmail.com>
2014-07-10 21:04:07 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,24 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Michal Wierzbinski on 10/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+- (id)init
+{
+    self = [super init];
+    if (self)
+    {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+
+@end
