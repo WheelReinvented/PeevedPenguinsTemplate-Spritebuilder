3b919c3a39be722cd06177fa6da5422c2341a528
Michal Wierzbinski <michal.wierzbinski@gmail.com>
2014-07-13 18:11:04 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Michal Wierzbinski on 13/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,13 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Michal Wierzbinski on 13/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay
+
+@end
