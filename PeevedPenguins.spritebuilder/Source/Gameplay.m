d679528051fbb03b8b19b2ab897ae7c2be4a9630
Michal Wierzbinski <michal.wierzbinski@gmail.com>
2014-07-15 21:23:58 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -19,8 +19,7 @@
     [_levelNode addChild:level];
     
     _phisicsNode.debugDraw = TRUE;
-    _phisicsNode.pullbackNode;
-//    _pullbackNode.physicsBody.collisionMask = @[];
+    _pullbackNode.physicsBody.collisionMask = @[];
 }
 
 // called on every touch in this scene
