af2af1971a83c94f2b6aa0ad1b0cfa24c1f41b0d
Michal Wierzbinski <michal.wierzbinski@gmail.com>
2014-07-15 21:08:34 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -17,6 +17,8 @@
     // load levels
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    
+    _physicsNode.debugDraw = TRUE;
 }
 
 // called on every touch in this scene
