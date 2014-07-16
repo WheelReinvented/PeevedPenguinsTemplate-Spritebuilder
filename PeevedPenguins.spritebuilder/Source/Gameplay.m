d5de7e45596e8e0ef875a7d8b3cba11d220146d1
Michal Wierzbinski <michal.wierzbinski@gmail.com>
2014-07-16 19:31:11 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -43,7 +43,7 @@
                                                             anchorA:ccp(0, 0)
                                                             anchorB:ccp(34, 138)
                                                          restLength:0.f
-                                                          stiffness:3000.f
+                                                          stiffness:10000.f
                                                             damping:150.f];
         CCLOG(@"%@",_mouseJoint);
     }
