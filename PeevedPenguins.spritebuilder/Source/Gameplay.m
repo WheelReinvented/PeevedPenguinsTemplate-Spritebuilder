2ecdc537c94846468805d46a3c70d06105192974
Michal Wierzbinski <michal.wierzbinski@gmail.com>
2014-07-22 21:58:43 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -23,6 +23,7 @@
     _mouseJointNode.physicsBody.collisionMask = @[];
     
  _phisicsNode.debugDraw = TRUE;
+    _phisicsNode.collisionDelegate = self;
     
 }
 
@@ -118,6 +119,11 @@
 - (void)retry {
     // reload this level
     [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
+}
+
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB
+{
+    CCLOG(@"Something collided with a seal!");
 }
 
 @end
