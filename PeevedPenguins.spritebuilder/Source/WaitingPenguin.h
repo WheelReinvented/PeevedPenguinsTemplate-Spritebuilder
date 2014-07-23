884214d2ef7894d236430d679df1c81ecf7363d4
Michal Wierzbinski <michal.wierzbinski@gmail.com>
2014-07-23 22:24:22 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Michal Wierzbinski on 23/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
